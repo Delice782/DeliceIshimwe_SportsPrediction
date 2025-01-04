373c19da35a3111ece334c7968f064f174e73d8a
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-01-04 00:07:46 +0000

    Update DeliceIshimwe_SportsPrediction (1).py

--- DeliceIshimwe_SportsPrediction (1).py
+++ DeliceIshimwe_SportsPrediction (1).py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
